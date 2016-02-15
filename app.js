3b3920c48fc9dd3732fbb34e174cf6d654f3b2ef
Seth Bergman <seth.atxwebs@gmail.com>
2016-02-15 14:17:38 -0600

    Pruning Dependencies

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // Database
 var mongo = require('mongodb');
-var db = monk('localhost:27017/test');
+var mongoose = require('mongoose');
+var db = mongoose('localhost:27017/test');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
